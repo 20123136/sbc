4547a913450dc66f070ac73fc2ecca53b05ca5b5
zhangna <zhangna@wilcom.com.cn>
2019-03-28 10:38:06 +0800

    localhost

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOSTlocalhost
+    host: 'localhost', // can be overwritten by process.env.HOST localhost
     port: 8089, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
